61c069712ab7fe6cc8b598d7954a6411d012eb8c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-28 22:10:52 +0000

    Commit

--- Java/src/main/java/com/vankosoft/myprojects/TomcatController.java
+++ Java/src/main/java/com/vankosoft/myprojects/TomcatController.java
@@ -4,26 +4,24 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.*;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class TomcatController
 {	
-	@GetMapping( "/" )
-    public Collection<String> sayHelloTomcat()
-    {
-        return IntStream.range( 0, 10 )
-          .mapToObj( i -> "Hello Tomcat number " + i )
-          .collect( Collectors.toList() );
-    }
+// 	@GetMapping( "/" )
+//     public Collection<String> sayHelloTomcat()
+//     {
+//         return IntStream.range( 0, 10 )
+//           .mapToObj( i -> "Hello Tomcat number " + i )
+//           .collect( Collectors.toList() );
+//     }
     
-    @GetMapping( "/myprojects/" )
-    public Collection<String> sayHello()
+    @GetMapping( "/hello" )
+    public String sayHello()
     {
-        return IntStream.range( 0, 10 )
-          .mapToObj( i -> "Hello number " + i )
-          .collect( Collectors.toList() );
+        return "Hello world!";
     }
 }
