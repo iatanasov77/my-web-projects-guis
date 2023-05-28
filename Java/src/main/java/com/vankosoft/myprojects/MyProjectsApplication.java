382fea17392aff869ed690ff17ff1c89671f0d98
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-28 13:01:32 +0000

    Code Refactoring of Java GUI.

--- Java/src/main/java/com/vankosoft/myprojects/MyProjectsApplication.java
+++ Java/src/main/java/com/vankosoft/myprojects/MyProjectsApplication.java
@@ -1,23 +1,23 @@
 package com.vankosoft.myprojects;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-
-
-//import org.springframework.boot.web.support.SpringBootServletInitializer;
-//For SpringBoot 2.0
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MyProjectsApplication extends SpringBootServletInitializer {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyProjectsApplication.class, args);
+@SpringBootApplication
+public class MyProjectsApplication extends SpringBootServletInitializer
+{
+	public static void main( String[] args )
+	{
+		SpringApplication.run( MyProjectsApplication.class, args );
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure( SpringApplicationBuilder builder )
+    {
+        return builder.sources( MyProjectsApplication.class );
+    }
 
 }
